a41b9bb7b71eb5af815b255e8e65e8c6334ddded
anneraev <47757104+anneraev@users.noreply.github.com>
2019-04-22 20:26:21 -0500

    created API

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -7,34 +7,6 @@ import OwnersList from "./owner/OwnersList";
 
 export default class ApplicationViews extends Component {
 
-    employeesFromAPI = [
-        { id: 1, name: "Jessica Younker" },
-        { id: 2, name: "Jordan Nelson" },
-        { id: 3, name: "Zoe LeBlanc" },
-        { id: 4, name: "Blaise Roberts" }
-    ]
-
-    locationsFromAPI = [
-        { id: 1, name: "Nashville North", address: "500 Circle Way" },
-        { id: 2, name: "Nashville South", address: "10101 Binary Court" }
-    ]
-
-    animalsFromAPI = [
-        { id: 1, name: "Doodles" },
-        { id: 2, name: "Jack" },
-        { id: 3, name: "Angus" },
-        { id: 4, name: "Henley" },
-        { id: 5, name: "Derkins" },
-        { id: 6, name: "Checkers" }
-    ]
-
-    ownersFromAPI = [
-        {id: 1, name: "Fred"},
-        {id: 2, name: "Anne"},
-        {id: 3, name: "Rocko"},
-        {id: 4, name: "Billie"}
-    ]
-
     state = {
         employees: this.employeesFromAPI,
         locations: this.locationsFromAPI,
