7dd6e6f857680495619429e203e2bd180d8c1c4a
anneraev <47757104+anneraev@users.noreply.github.com>
2019-05-13 00:43:51 -0500

    chapter 6

---
+++ src/components/Employee/employeeAPI.js
@@ -0,0 +1,23 @@
+import ApiCall from "../API"
+
+const key = "employees"
+//gets newState property from ApplicationViews in order to set a property inside it to the contents of the dataset.
+
+export default {
+    getAll: () => {
+        return ApiCall.getAll(key)
+    },
+    getOne: (id) => {
+        return ApiCall.getOne(key, id)
+    },
+    post: (object) => {
+        return ApiCall.post(key, object)
+    },
+    delete: (id) => {
+        return ApiCall.delete(key, id)
+    },
+    patch: (id, object) => {
+        return ApiCall.patch(key, id, object)
+    }
+
+}
